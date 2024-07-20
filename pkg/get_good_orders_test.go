cef67027e6e10e558aa6360cf2f19f5803155165
FreePhoenix888 <freephoenix888@gmail.com>
2024-07-20 23:56:12 +0500

    Do not use logger

--- pkg/get_good_orders_test.go
+++ pkg/get_good_orders_test.go
@@ -1,24 +1,8 @@
 package warframe_market_prime_trash_buyer
 
 import (
-	"os"
 	"testing"
-
-	"go.uber.org/zap"
 )
-
-var logger *zap.Logger
-
-func TestMain(m *testing.M) {
-	var err error
-	testLogger, err := zap.NewDevelopment()
-	if err != nil {
-		panic(err)
-	}
-	code := m.Run()
-	testLogger.Sync()
-	os.Exit(code)
-}
 
 func TestGetProfitableOrders(t *testing.T) {
 	profitableOrders, err := GetProfitableOrders()
